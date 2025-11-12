6919d802a37ea9d3373766a8989d3b3927a63124
Mikhail Kolesnikov <redrumrack@gmail.com>
2025-11-13 00:01:16 +0300

    Implement Vector class

--- src/game/player.js
+++ src/game/player.js
@@ -1,3 +1,4 @@
+import {Vector} from './vector'
 import {CharacterSheet} from './character-sheet'
 
 export class Player {
@@ -5,43 +6,43 @@ export class Player {
     x = 0
     y = 0
     speed = 70
+    velocity = new Vector('down', 0)
+    animationsSheet = new CharacterSheet('player', 576, 256)
+    animations = {}
 
     constructor(control) {
-        this.control = control
-        this.animations = {}
-        this.animationsSheet = new CharacterSheet('player', 576, 256);
+        this.control = control;
         ['walk_up', 'walk_down', 'walk_left', 'walk_right'].forEach(name => {
-            this.animations[name] = this.animationsSheet.getAnimation(name, 200, true, true)
+            this.animations[name] = this.animationsSheet.getAnimation(name, 250, true, true)
         })
-        this.view = this.animations['walk_down']
+        this.#stand('down')
     }
 
     update(timestamp) {
-        if (this.control.up) {
-            this.view = this.animations['walk_up']
-            this.y += (timestamp - this.lastTimestamp) * (-this.speed / 1000)
-            this.view.start()
-        }
-        else if (this.control.down) {
-            this.view = this.animations['walk_down']
-            this.y += (timestamp - this.lastTimestamp) * (this.speed / 1000)
-            this.view.start()
-        }
-        else if (this.control.left) {
-            this.view = this.animations['walk_left']
-            this.x += (timestamp - this.lastTimestamp) * (-this.speed / 1000)
-            this.view.start()
-        }
-        else if (this.control.right) {
-            this.view = this.animations['walk_right']
-            this.x += (timestamp - this.lastTimestamp) * (this.speed / 1000)
-            this.view.start()
-        } else {
-            this.view.stop()
-        }
+        if (!this.lastTimestamp) this.lastTimestamp = timestamp
 
+        if (this.control.up) this.#walk('up')
+        else if (this.control.down) this.#walk('down')
+        else if (this.control.left) this.#walk('left')
+        else if (this.control.right) this.#walk('right')
+        else this.#stand(this.velocity.direction)
+
+        this.x += (timestamp - this.lastTimestamp) * (this.velocity.x / 1000)
+        this.y += (timestamp - this.lastTimestamp) * (this.velocity.y / 1000)
         this.view.setXY(Math.trunc(this.x), Math.trunc(this.y))
         this.lastTimestamp = timestamp
         this.view.update(timestamp)
+    }
+
+    #walk(direction) {
+        this.velocity.setDirection(direction, this.speed)
+        this.view = this.animations['walk_' + direction]
+        this.view.start()
+    }
+
+    #stand(direction) {
+        this.velocity.setDirection(direction, 0)
+        this.view = this.animations['walk_' + direction]
+        this.view.stop()
     }
 }
